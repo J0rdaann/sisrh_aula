83ead64c0cf761b9562802d67855cd36ed262db9
Jordan Dias <jordanvdias@gmail.com>
2023-07-26 20:25:45 -0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+'<?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
