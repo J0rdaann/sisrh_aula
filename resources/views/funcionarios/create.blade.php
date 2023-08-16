0c5487d5d530fae9fae7766ce3fd2ba41aeb6dd2
Jordan Dias <jordanvdias@gmail.com>
2023-08-16 20:03:06 -0300

    Versao UP

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.default')
 
+@extends('layouts.default')
 @section('title', 'SisRH - Cadastro de Funcionário')
 
 @section('content')
@@ -8,47 +8,49 @@
     <form class="row g-3">
         <div class="col-md-6">
           <label for="nome" class="form-label">Nome</label>
-          <input type="text" class="form-control form-control-lg" id="nome" name="nome">
+          <input type="text" class="form-control" id="nome" name="nome" required>
         </div>
         <div class="col-md-6">
-          <label for="data_nasc" class="form-label">Dada de Nascimento</label>
+          <label for="data_nasc" class="form-label">Data de Nascimento</label>
           <input type="date" class="form-control" id="data_nasc" name="data_nasc" required>
         </div>
         <div class="col-md-4">
-          <label for="sexo" class="form-label">Sexo</label>
-          <select id="sexo" name="sexo" class="form-select" required>
+            <label for="sexo" class="form-label">Sexo</label>
+            <select id="sexo" name="sexo" class="form-select" required>
                 <option value=""></option>
                 <option value="m">Masculino</option>
                 <option value="f">Feminino</option>
                 <option value="o">Outros</option>
-          </select>
-        </div>
+            </select>
+          </div>
         <div class="col-md-4">
           <label for="cpf" class="form-label">CPF</label>
-          <input type="text" class="form-control" id="cpf" name="cpf" maxlength="11" minlength="11" required>
+          <input type="text" class="form-control" id="cpf" name="cpf" minlength="11" maxlength="11" required>
         </div>
         <div class="col-md-4">
-          <label for="email" class="form-label">E-mail</label>
-          <input type="emaiç" class="form-control" id="email" name="email" required>
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" name="email" required>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
           <label for="telefone" class="form-label">Telefone</label>
           <input type="text" class="form-control" id="telefone" name="telefone" required>
         </div>
         <div class="col-md-4">
-          <label for="departamento_id" class="form-label">Departamento</label>
+          <label for="departamento_id" class="form-label">Departamentos</label>
           <select id="departamento_id" name="departamento_id" class="form-select" required>
+            <option value=""></option>
             <option value="">Marketing</option>
             <option value="">Tecnologia</option>
           </select>
         </div>
         <div class="col-md-4">
-          <label for="cargo_id" class="form-label">Cargos</label>
-          <select id="cargo_id" name="cargo_id" class="form-select" required>
-            <option value="">Gerente</option>
-            <option value="">Superfisor</option>
-          </select>
-        </div>
+            <label for="cargo_id" class="form-label">Cargos</label>
+            <select id="cargo_id" name="cargo_id" class="form-select" required>
+              <option value=""></option>
+              <option value="">Gerente</option>
+              <option value="">Supervisor</option>
+            </select>
+          </div>
         <div class="col-md-4">
           <label for="salario" class="form-label">Salário</label>
           <input type="text" class="form-control" id="salario" name="salario" required>
@@ -59,17 +61,15 @@
         </div>
         <div class="col-md-4">
           <label for="data_desligamento" class="form-label">Data de Desligamento</label>
-          <input type="date" class="form-control" id="data_desligamento" name="data_desligamento" >
+          <input type="date" class="form-control" id="data_desligamento" name="data_desligamento">
         </div>
         <div class="col-12">
             <label for="foto" class="form-label">Foto</label>
             <input type="file" class="form-control" id="foto" name="foto">
-          </div>
         </div>
         <div class="col-12">
-          <button type="submit" class="btn btn-primary btn-lg">Cadastrar</button>
+          <button type="submit" class="btn btn-success">Cadastrar</button>
           <a href="{{ route('funcionarios.index') }}" class="btn btn-danger">Cancelar</a>
         </div>
       </form>
-
 @endsection
