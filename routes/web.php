080843413bccacb1dd00b83ff4785051f832b19c
Jordan Dias <jordanvdias@gmail.com>
2023-09-13 02:00:39 -0300

    Correções de erros

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,7 @@ Route::get('/', function () {
     return view('login');
 });
 
-/*Route::get('/funcionarios', function () {
-    return view('funcionarios.index');
-});*/
-
+//FUNCIONARIO
 Route::get('/funcionarios', [FuncionarioController::class, 'index'])->name('funcionarios.index');
 Route::get('/funcionarios/create', [FuncionarioController::class, 'create'])->name('funcionarios.create');
 Route::post('/funcionarios', [FuncionarioController::class, 'store'])->name('funcionarios.store');
@@ -33,18 +30,23 @@ Route::get('/funcionarios/{id}/edit', [FuncionarioController::class, 'edit'])->n
 Route::put('/funcionarios/{id}', [FuncionarioController::class, 'update'])->name('funcionarios.update');
 Route::delete('/funcionarios/{id}/delete', [FuncionarioController::class, 'destroy'])->name('funcionarios.destroy');
 
+//DEPARTAMENTO
 Route::get('/departamentos', [DepartamentoController::class, 'index'])->name('departamentos.index');
 Route::post('/departamentos', [DepartamentoController::class, 'store'])->name('departamentos.store');
 Route::get('/departamentos/create', [DepartamentoController::class, 'create'])->name('departamentos.create');
 Route::put('/departamentos/{id}', [DepartamentoController::class, 'update'])->name('departamentos.update');
 Route::get('/departamentos/{id}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
 
+
+//CARGO
 Route::get('/cargos', [CargoController::class, 'index'])->name('cargos.index');
 Route::post('/cargos', [CargoController::class, 'store'])->name('cargos.store');
 Route::get('/cargos/create', [CargoController::class, 'create'])->name('cargos.create');
 Route::put('/cargos/{id}', [CargoController::class, 'update'])->name('cargos.update');
 Route::get('/cargos/{id}/edit', [CargoController::class, 'edit'])->name('cargos.edit');
 
+
+//USUÁRIOS
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
